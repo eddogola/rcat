c3810a5759461db7a9d44b0d252b7c24077fd9a2
Eddy Ogola <eddy.ogola1@gmail.com>
2022-07-04 09:20:22 -0700

    Show custom is directory error message

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
-use std::io::ErrorKind;
+use std::io::Read;
+use std::path::Path;
+use std::io::BufReader;
 
 fn main() {
     // use env::args to panic when invalid unicode is entered
@@ -15,14 +17,22 @@ fn main() {
         // show the error and end the process
         
         for filename in &filenames[1..] {
-            match fs::read_to_string(filename) {
-                Ok(content) => println!("{}", content),
-                Err(e) => match e.kind() {
-                    // file/directory not found
-                    ErrorKind::NotFound => eprintln!("rcat: {filename}: No such file or directory", filename=filename),
-                    // is a directory
-                    // ErrorKind::IsADirectory => eprintln!("rcat: {filename}: Is a directory", filename=filename),
-                    _ => println!("{}", e),
+            let path = Path::new(filename);
+            if !path.exists() {
+                eprintln!("rcat: {}: No such file or directory", filename);
+            } else if path.is_dir() {
+                eprintln!("rcat: {}: Is a directory", filename);
+            } else {
+                match fs::File::open(path) {
+                    Ok(file) => {
+                        let mut buf_reader = BufReader::new(file);
+                        let mut content = String::new();
+                        if let Err(e) = buf_reader.read_to_string(&mut content) {
+                            eprintln!("{}", e);
+                        }
+                        println!("{}", content.trim());
+                    },
+                    Err(e) => eprintln!("{}", e),
                 }
             }
         }
